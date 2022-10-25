2478bf9857e41948e49a29dd93915e1679c11fa1
Mart-Dan-Rossi <94561321+Mart-Dan-Rossi@users.noreply.github.com>
2022-10-25 20:24:41 -0300

    Add css to rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import MainMenu from "./components/MainMenu";
 import Rules from "./components/Rules";
 import { GameContextProvider } from "./context/GameContextProvider";

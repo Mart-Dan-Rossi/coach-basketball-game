df8451dcccfb031888c72bc5798f9c1bb7a209dc
Mart-Dan-Rossi <94561321+Mart-Dan-Rossi@users.noreply.github.com>
2023-09-14 17:46:47 -0300

    Optimise code in gameContextProvider

--- src/context/GameContextProvider.tsx
+++ src/context/GameContextProvider.tsx
@@ -1,6 +1,7 @@
 import { GameContext } from "./GameContext"
 import React from 'react'
 import {useState} from 'react';
+import {getMinStatPerPosition} from "../utilities/exportableFunctions"
 
 interface props {
     children: JSX.Element | JSX.Element[]
@@ -8,79 +9,74 @@ interface props {
 
 export const GameContextProvider = ({ children }: props) => {
 
-    //Use the min values per position to fill this info
     const [playerA1Stats, setPlayerA1Stats] = useState({
         name: "",
         position: "1",
-        height: 165,
-        weight: 60,
-        atleticism: 60,
-        perimeterDefence: 50,
-        insideDefence: 10,
-        rebounding: 10,
-        perimeterScoring: 50,
-        insideScoring: 30,
-        playMaking: 40
+        height: getMinStatPerPosition("height", "1"),
+        weight: getMinStatPerPosition("weight", "1"),
+        atleticism: getMinStatPerPosition("atleticism", "1"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "1"),
+        insideDefence: getMinStatPerPosition("insideDefence", "1"),
+        rebounding: getMinStatPerPosition("rebounding", "1"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "1"),
+        insideScoring: getMinStatPerPosition("insideScoring", "1"),
+        playMaking: getMinStatPerPosition("playMaking", "1")
     })
 
-    //Use the min values per position to fill this info
     const [playerA2Stats, setPlayerA2Stats] = useState({
         name: "",
         position: "2",
-        height: 170,
-        weight: 65,
-        atleticism: 40,
-        perimeterDefence: 45,
-        insideDefence: 15,
-        rebounding: 15,
-        perimeterScoring: 50,
-        insideScoring: 30,
-        playMaking: 40
+        height: getMinStatPerPosition("height", "2"),
+        weight: getMinStatPerPosition("weight", "2"),
+        atleticism: getMinStatPerPosition("atleticism", "2"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "2"),
+        insideDefence: getMinStatPerPosition("insideDefence", "2"),
+        rebounding: getMinStatPerPosition("rebounding", "2"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "2"),
+        insideScoring: getMinStatPerPosition("insideScoring", "2"),
+        playMaking: getMinStatPerPosition("playMaking", "2")
     })   
 
-    //Use the min values per position to fill this info
     const [playerA3Stats, setPlayerA3Stats] = useState({
         name: "",
         position: "3",
-        height: 175,
-        weight: 70,
-        atleticism: 40,
-        perimeterDefence: 40,
-        insideDefence: 30,
-        rebounding: 30,
-        perimeterScoring: 30,
-        insideScoring: 40,
-        playMaking: 10
+        height: getMinStatPerPosition("height", "3"),
+        weight: getMinStatPerPosition("weight", "3"),
+        atleticism: getMinStatPerPosition("atleticism", "3"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "3"),
+        insideDefence: getMinStatPerPosition("insideDefence", "3"),
+        rebounding: getMinStatPerPosition("rebounding", "3"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "3"),
+        insideScoring: getMinStatPerPosition("insideScoring", "3"),
+        playMaking: getMinStatPerPosition("playMaking", "3")
     })    
 
-    //Use the min values per position to fill this info
     const [playerA4Stats, setPlayerA4Stats] = useState({
         name: "",
         position: "4",
-        height: 190,
-        weight: 80,
-        atleticism: 30,
-        perimeterDefence: 20,
-        insideDefence: 40,
-        rebounding: 50,
-        perimeterScoring: 10,
-        insideScoring: 60,
-        playMaking: 5
+        height: getMinStatPerPosition("height", "4"),
+        weight: getMinStatPerPosition("weight", "4"),
+        atleticism: getMinStatPerPosition("atleticism", "4"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "4"),
+        insideDefence: getMinStatPerPosition("insideDefence", "4"),
+        rebounding: getMinStatPerPosition("rebounding", "4"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "4"),
+        insideScoring: getMinStatPerPosition("insideScoring", "4"),
+        playMaking: getMinStatPerPosition("playMaking", "4")
     })
 
-    //Use the min values per position to fill this info
     const [playerA5Stats, setPlayerA5Stats] = useState({
         name: "",
         position: "5",
-        height: 205,
-        weight: 90,
-        atleticism: 15,
-        perimeterDefence: 5,
-        insideDefence: 60,
-        rebounding: 60,
-        perimeterScoring: 5,
-        insideScoring: 65,
-        playMaking: 1
+        height: getMinStatPerPosition("height", "5"),
+        weight: getMinStatPerPosition("weight", "5"),
+        atleticism: getMinStatPerPosition("atleticism", "5"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "5"),
+        insideDefence: getMinStatPerPosition("insideDefence", "5"),
+        rebounding: getMinStatPerPosition("rebounding", "5"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "5"),
+        insideScoring: getMinStatPerPosition("insideScoring", "5"),
+        playMaking: getMinStatPerPosition("playMaking", "5")
     }) 
 
     const [teamAStats, setTeamAStats] = useState({
@@ -91,79 +87,74 @@ export const GameContextProvider = ({ children }: props) => {
         playerA5Stats
     })
 
-    //Use the min values per position to fill this info
     const [playerB1Stats, setPlayerB1Stats] = useState({
         name: "",
         position: "1",
-        height: 165,
-        weight: 60,
-        atleticism: 60,
-        perimeterDefence: 50,
-        insideDefence: 10,
-        rebounding: 10,
-        perimeterScoring: 50,
-        insideScoring: 30,
-        playMaking: 40
+        height: getMinStatPerPosition("height", "1"),
+        weight: getMinStatPerPosition("weight", "1"),
+        atleticism: getMinStatPerPosition("atleticism", "1"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "1"),
+        insideDefence: getMinStatPerPosition("insideDefence", "1"),
+        rebounding: getMinStatPerPosition("rebounding", "1"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "1"),
+        insideScoring: getMinStatPerPosition("insideScoring", "1"),
+        playMaking: getMinStatPerPosition("playMaking", "1")
     })
 
-    //Use the min values per position to fill this info
     const [playerB2Stats, setPlayerB2Stats] = useState({
         name: "",
         position: "2",
-        height: 170,
-        weight: 65,
-        atleticism: 40,
-        perimeterDefence: 45,
-        insideDefence: 15,
-        rebounding: 15,
-        perimeterScoring: 50,
-        insideScoring: 30,
-        playMaking: 40
+        height: getMinStatPerPosition("height", "2"),
+        weight: getMinStatPerPosition("weight", "2"),
+        atleticism: getMinStatPerPosition("atleticism", "2"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "2"),
+        insideDefence: getMinStatPerPosition("insideDefence", "2"),
+        rebounding: getMinStatPerPosition("rebounding", "2"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "2"),
+        insideScoring: getMinStatPerPosition("insideScoring", "2"),
+        playMaking: getMinStatPerPosition("playMaking", "2")
     })   
 
-    //Use the min values per position to fill this info
     const [playerB3Stats, setPlayerB3Stats] = useState({
         name: "",
         position: "3",
-        height: 175,
-        weight: 70,
-        atleticism: 40,
-        perimeterDefence: 40,
-        insideDefence: 30,
-        rebounding: 30,
-        perimeterScoring: 30,
-        insideScoring: 40,
-        playMaking: 10
+        height: getMinStatPerPosition("height", "3"),
+        weight: getMinStatPerPosition("weight", "3"),
+        atleticism: getMinStatPerPosition("atleticism", "3"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "3"),
+        insideDefence: getMinStatPerPosition("insideDefence", "3"),
+        rebounding: getMinStatPerPosition("rebounding", "3"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "3"),
+        insideScoring: getMinStatPerPosition("insideScoring", "3"),
+        playMaking: getMinStatPerPosition("playMaking", "3")
     })    
 
-    //Use the min values per position to fill this info
     const [playerB4Stats, setPlayerB4Stats] = useState({
         name: "",
         position: "4",
-        height: 190,
-        weight: 80,
-        atleticism: 30,
-        perimeterDefence: 20,
-        insideDefence: 40,
-        rebounding: 50,
-        perimeterScoring: 10,
-        insideScoring: 60,
-        playMaking: 5
+        height: getMinStatPerPosition("height", "4"),
+        weight: getMinStatPerPosition("weight", "4"),
+        atleticism: getMinStatPerPosition("atleticism", "4"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "4"),
+        insideDefence: getMinStatPerPosition("insideDefence", "4"),
+        rebounding: getMinStatPerPosition("rebounding", "4"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "4"),
+        insideScoring: getMinStatPerPosition("insideScoring", "4"),
+        playMaking: getMinStatPerPosition("playMaking", "4")
     })
 
-    //Use the min values per position to fill this info
     const [playerB5Stats, setPlayerB5Stats] = useState({
         name: "",
         position: "5",
-        height: 205,
-        weight: 90,
-        atleticism: 15,
-        perimeterDefence: 5,
-        insideDefence: 60,
-        rebounding: 60,
-        perimeterScoring: 5,
-        insideScoring: 65,
-        playMaking: 1
+        height: getMinStatPerPosition("height", "5"),
+        weight: getMinStatPerPosition("weight", "5"),
+        atleticism: getMinStatPerPosition("atleticism", "5"),
+        perimeterDefence: getMinStatPerPosition("perimeterDefence", "5"),
+        insideDefence: getMinStatPerPosition("insideDefence", "5"),
+        rebounding: getMinStatPerPosition("rebounding", "5"),
+        perimeterScoring: getMinStatPerPosition("perimeterScoring", "5"),
+        insideScoring: getMinStatPerPosition("insideScoring", "5"),
+        playMaking: getMinStatPerPosition("playMaking", "5")
     }) 
 
     const [teamBStats, setTeamBStats] = useState({
